ab3bf2d7dc665fe816e68d10b262dc132e29f8e0
TheLoserCoder <podnebesnyy11@gmail.com>
2020-08-08 00:50:22 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+
+app.use('/', express.static(__dirname + '../public'));
+
+app.get('/*', function(req, res){
+  res.status(404);
+  res.send('Такой страницы не существует')
+});
+ 
+
+app.listen(process.env.PORT || 5000);

23dbab465f645380e27366b3e6db76753450bf41
TheLoserCoder <podnebesnyy11@gmail.com>
2020-08-09 16:45:57 +0300

    This is a new commit for what I originally planned to be amended

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express();
 
+
+
 app.use('/', express.static(__dirname + './../public'));
+
+
 
 app.get('/*', function(req, res){
   res.status(404);
@@ -9,4 +13,6 @@ app.get('/*', function(req, res){
 });
  
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000), () => {
+  console.log("Server is runing..")
+};
